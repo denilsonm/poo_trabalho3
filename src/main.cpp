514bc5cc0170420137327ad14045259b75e2a08e
Lucas Mello <chapeutyler@gmail.com>
2014-12-06 17:20:09 -0200

    Implementação básica do torneio com criação dos times e personagens na main

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 #include <cstdlib>
 
 #include "GameUtil.hpp"
+#include "Character.hpp"
+#include "Knight.hpp"
+#include "Wizard.hpp"
+#include "Archer.hpp"
 #include "Tournament.hpp"
+#include "Color.hpp"
 
 using namespace std;
 
@@ -44,16 +49,119 @@ int main(){
 
     // Lendo a classe desejada
 
-    string playerClass;
+    int playerClass;
 
     do{
-        cout << "Pick your class (Knight/Wizard/Archer): ";
+        cout << "Pick your class (1 - Knight/2 - Wizard/3 - Archer): ";
         cin >> playerClass;
-    }while(playerClass != "Knight" && playerClass != "Wizard" && playerClass != "Archer")
+    }while(playerClass < 1 || playerClass > 3)
 
-    cout << "\nYou have received the following items:\n\n"
+    // Cria o personagem do jogador
+    Character player;
 
-    //
+    switch(playerClass)
+    {
+        case 1: // Knight
+            player = new Knight(playerName, 100, 0, 1);
+            break;
+        case 2: // Wizard
+            player = new Wizard(playerName, 70, 50, 1);
+            break;
+        case 3: // Archer
+            player = new Archer(playerName, 80, 0, 1);
+            break;
+        default:
+            break;
+    }
+
+    cout << "\nYou have received the following items:\n\n";
+    // TODO start kit
 
     cout << "May the bravest of all win!";
+
+    // Cria um vetor de parties participantes do torneio
+    vector<Party*> partyList;
+
+    // Cria o time do jogador
+    Party party = new Party("TODO", TODO);
+
+    // Coloca o jogador no time
+    party.addChar(player);
+    
+    // Cria os NPC's do time do personagem do jogador
+    for(unsigned int i = 0; i < 2; i++)
+    {
+        int npcClass = GameUtil::generateRandom(1, 3);
+
+        switch(npcClass)
+        {
+            case 1: // Knight
+                party.addChar(new Knight("TODO", 100, 0, 1));
+                break;
+            case 2: // Wizard
+                party.addChar(new Wizard("TODO", 70, 50, 1));
+                break;
+            case 3: // Archer
+                party.addChar(new Archer("TODO", 80, 0, 1));
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Coloca o time do personagem do jogador na lista para o torneio
+    partyList.push_back(party);
+
+    // Cria os times inimigos
+    for(unsigned int i = 0; i < 7; i++)
+    {
+        party = new Party("TODO", TODO);
+
+        // Cria os NPC's dos outros times
+        for(unsigned int i = 0; i < 3; i++)
+        {
+            int npcClass = GameUtil::generateRandom(1, 3);
+
+            switch(npcClass)
+            {
+                case 1: // Knight
+                    party.addChar(new Knight(playerName, 100, 0, 1));
+                    break;
+                case 2: // Wizard
+                    party.addChar(new Wizard(playerName, 70, 50, 1));
+                    break;
+                case 3: // Archer
+                    party.addChar(new Archer(playerName, 80, 0, 1));
+                    break;
+                default:
+                    break;
+            }
+        }
+    
+        // Coloca os times inimigos na lista para o torneio
+        partyList.push_back(party);
+    }
+
+    // Cria um torneio e coloca a lista de parties
+    Tournament tournament(partyList);
+
+    // Inicia o torneio
+    while(tournament.getWinner() == NULL) // TODO verificar se jogador morreu em algum lugar
+    {
+        // Executa um round do torneio
+        tournament.startRound();
+
+        // Abre a loja para o jogador
+        //TODO shop
+    }
+
+    // Deleta as parties que participaram do torneio
+    for(unsigned int i = 0; i < partyList.size(); i++)
+    {
+        delete partyList.at(i);
+    }
+    partyList.erase();
+
+    // Deleta a memória usada pelo jogador
+    delete player;
 }
